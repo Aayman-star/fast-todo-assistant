0a6f974e978340783337c3ad089d3fd5709219a2
Aayman-star <aayman.khalid1@gmail.com>
2024-02-21 17:34:33 +0500

    created table for users and token

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
 """"This file is to create database models"""
 import os
 from sqlalchemy import create_engine, Column, Integer, String,Boolean
+from sqlalchemy.sql import func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv,find_dotenv
 from sqlmodel import SQLModel, Field, Relationship, Session, select,MetaData
 from typing import Optional,Union
+import datetime
 
 """Loading the environment variable"""
 load_dotenv(override=True)
@@ -15,11 +17,29 @@ dotenv_file = find_dotenv()
 DATABASE_URL = os.environ["DATABASE_URL"]
 print(DATABASE_URL)
 
+#This is the table for user data
+class User(SQLModel,table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    username: str
+    useremail: str
+    password:str
+    #created_at: datetime.datetime = Field(default_factory=datetime.datetime.now)
 
+#This is the class to create the user from the client side
+class UserCreate(SQLModel):
+    username: str
+    useremail: str
+    password:str
+
+class Token(SQLModel):
+    access_token: str
+    token_type: str
+#This is the table for todo data
 class Todo_Assistant(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     text: str
     is_complete: bool = False
+    user_id: int = Field(foreign_key="user.id") #This is the foreign key to identify the user,the id comes from the user table
     
 
 class TodoCreate(SQLModel):
